ce8d82efe6bf9d21b30614eee251080a31e4e048
JohanRengifo <johanstivenrengifo@outlook.com>
2024-05-28 21:05:48 -0500

    Fix Bugs🐛 #006

--- update.php
+++ update.php
@@ -2,15 +2,26 @@
 // Ruta de Trabajo
 $repo_dir = 'FacialSystem-alpha-server';
 
-// Ejecutar los comandos de Git para actualizar
-exec("cd $repo_dir && git pull");
+// Leer los datos del cuerpo de la solicitud POST
+$data = json_decode(file_get_contents('php://input'), true);
 
-// Instalar dependencias 
-exec("cd $repo_dir && npm install"); 
+// Verificar si se trata de un evento "push"
+if ($data['ref'] === 'refs/heads/main') { 
+    // Ejecutar los comandos de Git para actualizar
+    exec("cd $repo_dir && git pull");
 
-// Reiniciar el servicio usando PM2
-exec("cd $repo_dir && pm2 restart ecosystem.config.js");
+    // Instalar dependencias
+    exec("cd $repo_dir && npm install");
 
-echo 'Cambios Cargados Correctamente';
+    // Reiniciar el servicio usando PM2
+    exec("cd $repo_dir && pm2 restart ecosystem.config.js");
 
+    // Enviar una respuesta al webhook
+    http_response_code(200); // Código de éxito
+    echo json_encode(array('message' => 'Actualización exitosa'));
+} else {
+    // Enviar una respuesta si no es un evento "push" a main
+    http_response_code(202); // Código de aceptación
+    echo json_encode(array('message' => 'No se requiere actualización'));
+}
 ?>
